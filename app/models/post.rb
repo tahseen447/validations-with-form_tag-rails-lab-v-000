d9ec590fbfa8bfab4154eb643729de23ec40e13e
tahseen447 Siddiqui <tahseenfatima1986@gmail.com>
2019-01-09 22:10:16 +0000

    Automatically backed up by Learn

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,5 +1,5 @@
 class Post < ActiveRecord::Base
   validates :title, presence: true
   validates :content, length: {minimum: 100}
-  validates :category, inlcusion: {'Fiction', 'Non-Fiction'}
+  validates :category, inlcusion: {in: %w(Fiction Non-Fiction)}
 end
