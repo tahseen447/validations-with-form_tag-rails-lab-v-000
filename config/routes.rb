a56ee07feed92dc4e33f340e00d96088eb855fcb
tahseen447 Siddiqui <tahseenfatima1986@gmail.com>
2019-01-09 23:00:34 +0000

    Done.

--- config/routes.rb
+++ config/routes.rb
@@ -53,4 +53,16 @@ Rails.application.routes.draw do
   #     # (app/controllers/admin/products_controller.rb)
   #     resources :products
   #   end
+
+  get "/authors/new", to: "authors#new", as: "new_author"
+  post "/authors", to: "authors#create"
+  get "/authors/:id", to: "authors#show", as: "author"
+  get "/authors/:id/edit", to: "authors#edit", as: "edit_author"
+  patch "/authors/:id", to: "authors#update"
+
+  get "/posts/new", to: "posts#new", as: "new_post"
+  post "/posts", to: "posts#create"
+  get "/posts/:id", to: "posts#show", as: "post"
+  get "/posts/:id/edit", to: "posts#edit", as: "edit_post"
+  patch "/posts/:id", to: "posts#update"
 end
