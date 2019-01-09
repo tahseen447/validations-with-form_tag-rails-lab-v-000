105f1b3a16213dfec051445f32adbf32cd5906e3
tahseen447 Siddiqui <tahseenfatima1986@gmail.com>
2019-01-09 22:53:17 +0000

    Automatically backed up by Learn

--- config/routes.rb
+++ config/routes.rb
@@ -59,4 +59,10 @@ Rails.application.routes.draw do
   get "/authors/:id", to: "authors#show", as: "author"
   get "/authors/:id/edit", to: "authors#edit", as: "edit_author"
   patch "/authors/:id", to: "authors#update"
+
+  get "/authors/new", to: "authors#new", as: "new_author"
+  post "/authors", to: "authors#create"
+  get "/authors/:id", to: "authors#show", as: "author"
+  get "/authors/:id/edit", to: "authors#edit", as: "edit_author"
+  patch "/authors/:id", to: "authors#update"
 end
