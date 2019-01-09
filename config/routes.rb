fa4fb1fb3acb96eb906df1f83a096245fc6a9a0b
tahseen447 Siddiqui <tahseenfatima1986@gmail.com>
2019-01-09 22:54:19 +0000

    Automatically backed up by Learn

--- config/routes.rb
+++ config/routes.rb
@@ -63,6 +63,6 @@ Rails.application.routes.draw do
   get "/posts/new", to: "posts#new", as: "new_post"
   post "/posts", to: "posts#create"
   get "/posts/:id", to: "posts#show", as: "post"
-  get "/posts/:id/edit", to: "posts#edit", as: "edit_author"
+  get "/posts/:id/edit", to: "posts#edit", as: "edit_post"
   patch "/posts/:id", to: "posts#update"
 end
